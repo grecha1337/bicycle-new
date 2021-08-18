ec4745dab1b841dc75658f205729a955afb5b593
Stas Sudovskiy <4rmor3d@gmail.com>
2021-08-19 00:11:13 +0300

    Исправлена работа слайдеров

--- js/slider.js
+++ js/slider.js
@@ -1,42 +1,38 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".road__slider", {
   // Optional parameters
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
-
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider__button-next',
-    prevEl: '.slider__button-prev',
+    nextEl: ".slider__button-next",
+    prevEl: ".slider__button-prev",
   },
-  slidesPerView : 2,
-  spaceBetween: 40, 
+  slidesPerView: 2,
+  spaceBetween: 40,
 });
 
-const cardsMobile = new Swiper('.cards-mobile', {
+const cardsMobile = new Swiper(".bicycle__cards-mobile", {
   // Optional parameters
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
-  slidesPerView : 1,
-  pagination : {
-    el: '.swiper-pagination'
+  slidesPerView: 1,
+  spaceBetween: 40,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
   },
-  observer: true,
-  observerParents: true,
-  observerSliderChildren: true,
 });
 
-
-const sliderMobile = new Swiper('.slider-mobile', {
+const sliderMobile = new Swiper(".slider-mobile", {
   // Optional parameters
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
-
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-mobile__button-next',
-    prevEl: '.slider-mobile__button-prev',
+    nextEl: ".slider-mobile__button-next",
+    prevEl: ".slider-mobile__button-prev",
   },
-  slidesPerView : 1,
+  slidesPerView: 1,
 });
